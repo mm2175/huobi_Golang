1df28b3d71ba4cb0d457b6e3b5f9c8502c1e4a6c
wangjunwei <wangjunwei87@gmail.com>
2021-06-06 00:48:08 +0800

    feat: add ratelimit log
    Change-Id: I9941372a59a3c753e79e0cc9937b4b7728ffe24f

--- internal/httprequest.go
+++ internal/httprequest.go
@@ -1,10 +1,12 @@
 package internal
 
 import (
-	"github.com/huobirdcenter/huobi_golang/logging/perflogger"
 	"io/ioutil"
 	"net/http"
 	"strings"
+
+	"github.com/huobirdcenter/huobi_golang/logging/applogger"
+	"github.com/huobirdcenter/huobi_golang/logging/perflogger"
 )
 
 func HttpGet(url string) (string, error) {
@@ -17,6 +19,8 @@ func HttpGet(url string) (string, error) {
 	}
 	defer resp.Body.Close()
 	result, err := ioutil.ReadAll(resp.Body)
+
+	logRateLimit(resp)
 
 	logger.StopAndLog("GET", url)
 
@@ -34,7 +38,19 @@ func HttpPost(url string, body string) (string, error) {
 	defer resp.Body.Close()
 	result, err := ioutil.ReadAll(resp.Body)
 
+	logRateLimit(resp)
 	logger.StopAndLog("POST", url)
 
 	return string(result), err
+}
+
+var counter = 0
+
+func logRateLimit(resp *http.Response) {
+	counter += 1
+	// just log, do not need precise here
+	if counter%100 == 0 {
+		applogger.Debug("Huobi RateLimit Remain: %s, Expire: %s",
+			resp.Header.Get("X-HB-RateLimit-Requests-Remain"), resp.Header.Get("X-HB-RateLimit-Requests-Expire"))
+	}
 }
